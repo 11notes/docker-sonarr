5845f27487de25d5ffc53fb90596cfa9b67f87c8
ElevenNotes <github@11notes.ch>
2026-07-23 12:16:49 +0200

    feat: add helm chart and terraform script

---
+++ terraform/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_version = ">= 1.15.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.2"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace_v1" "arr" {
+  metadata {
+    name = "arr"
+  }
+}
+
+resource "kubernetes_secret_v1" "postgres_password" {
+  metadata {
+    name = "sonarr-db-password"
+    namespace = "arr"
+  }
+
+  data = {
+    POSTGRES_PASSWORD = trimspace(var.postgres_password)
+  }
+
+  type = "Opaque"
+}
+
+resource "helm_release" "sonarr-db" {
+  name = "sonarr-db"
+  repository = "oci://ghcr.io/11notes/charts"
+  chart = "postgres"
+  namespace  = "arr"
+  version = "1.0.0"
+
+  values = [
+    yamlencode({
+      image = {
+        tag: "18"
+      }
+      postgres = {
+        existingSecret    = "sonarr-db-password"
+        existingSecretKey = "POSTGRES_PASSWORD"
+      }
+      persistence = {
+        etc = {
+          size = "16Mi"
+        }
+        var = {
+          size = "32Gi"
+        }
+      }
+    })
+  ]
+}
+
+resource "helm_release" "sonarr" {
+  name = "sonarr"
+  repository = "oci://ghcr.io/11notes/charts"
+  chart = "sonarr"
+  namespace  = "arr"
+  version = "1.0.0"
+  create_namespace = false
+
+  values = [
+    yamlencode({
+      image = {
+        tag: "4.0.19"
+      }
+      postgres = {
+        existingSecret = "sonarr-db-password"
+        existingSecretKey = "POSTGRES_PASSWORD"
+        serviceName = "sonarr-db"
+      }
+      persistence = {
+        etc = {
+          size = "8Gi"
+        }
+      }
+    })
+  ]
+}
